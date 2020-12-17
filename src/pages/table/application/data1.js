3916a94a6c53a7e64f449748fe06af8066ad24f1
majian1 <majian1@viewhigh.com>
2020-12-17 14:05:44 +0800

    [dev] 数据注释

--- src/pages/table/application/data1.js
+++ src/pages/table/application/data1.js
@@ -1,5 +1,11 @@
 export default [
-  { id: '0', name: null, source: '1', target: '0', lineStyle: { normal: {} } },
+  {
+    id: '0',
+    name: null,
+    source: '1', //指向子节点
+    target: '0', //原节点
+    lineStyle: { normal: {} },
+  },
   { id: '1', name: null, source: '2', target: '1', lineStyle: { normal: {} } },
   { id: '2', name: null, source: '3', target: '2', lineStyle: { normal: {} } },
 ];
