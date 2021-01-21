f7e05f8023c3a35d832ffa9f0d787abebc7f8395
majian1 <majian1@viewhigh.com>
2021-01-21 16:40:25 +0800

    [dev] bug修复

--- src/models/auth.js
+++ src/models/auth.js
@@ -9,6 +9,7 @@ const Model = {
   state: {},
   effects: {
     // *auth({ payload }, { select, call }) {
+    //   console.log('登录状态验证');
     //   const result = yield call(heartbeat);
     //   if (!result.data) {
     //     history.replace('/login');
